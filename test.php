65b143b1c91f4d13c5bb5bcf6cfd065424c46a8c
danbrincat <dan.brincat07@gmail.com>
2023-01-16 22:45:19 +0000

    Update test.php

--- test.php
+++ test.php
@@ -2,7 +2,7 @@
 $curl = curl_init();
 
 curl_setopt_array($curl, array(
-    CURLOPT_URL => 'https://api.dataforseo.com/v3/serp/google/organic/live/advanced',
+    CURLOPT_URL => 'https://api.dataforseo.com/v3/keywords_data/google_trends/explore/live',
     CURLOPT_RETURNTRANSFER => true,
     CURLOPT_ENCODING => '',
     CURLOPT_MAXREDIRS => 10,
@@ -11,10 +11,10 @@ curl_setopt_array($curl, array(
     CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
     CURLOPT_CUSTOMREQUEST => 'POST',
     CURLOPT_POSTFIELDS =>'[
-        {"keyword":"weather forecast", "location_code":2826, "language_code":"en", "device":"desktop", "os":"windows", "depth":100}
+        {"type":"web", "keywords":["hats"]}
     ]',
     CURLOPT_HTTPHEADER => array(
-        'Authorization: Basic ZGFuLmJyaW5jYXQwN0BnbWFpbC5jb206YTY5MzI2YzNmODRjNjEzZA==',
+        'Authorization: Basic ZGFuLmJyaW5jYXQwN0BnbWFpbC5jb206YTY5MzI2YzNmODRjNjEzZHBhc3N3b3Jk',
         'Content-Type: application/json'
     ),
 ));
