24bc5d143c1c5f77857086cbbd1804a504ddcb25
danbrincat <dan.brincat07@gmail.com>
2023-01-16 18:06:14 +0000

    Update test.php

--- test.php
+++ test.php
@@ -1,70 +1,25 @@
 <?php
-// You can download this file from here https://cdn.dataforseo.com/v3/examples/php/php_RestClient.zip
-require('RestClient.php');
-$api_url = 'https://api.dataforseo.com/';
-try {
-  // Instead of 'login' and 'password' use your credentials from https://app.dataforseo.com/api-dashboard
-  $client = new RestClient($api_url, null, 'dan.brincat07@gmail.com', 'a69326c3f84c613d');
-} catch (RestClientException $e) {
-  echo "\n";
-  print "HTTP code: {$e->getHttpCode()}\n";
-  print "Error code: {$e->getCode()}\n";
-  print "Message: {$e->getMessage()}\n";
-  print  $e->getTraceAsString();
-  echo "\n";
-  exit();
-}
-$post_array = array();
-// simple way to set a task
-$post_array[] = array(
-  "location_name" => "United States",
-  "date_from" => "2019-01-01",
-  "date_to" => "2020-01-01",
-  "keywords" => array(
-    "seo api"
-  )
-);
-// after a task is completed, we will send a GET request to the address you specify
-// instead of $id and $tag, you will receive actual values that are relevant to this task
-$post_array[] = array(
-  "location_name" => "United States",
-  "date_from" => "2019-01-01",
-  "date_to" => "2020-01-01",
-  "type" => "youtube",
-  "category_code" => 3,
-  "keywords" => array(
-    "seo api",
-    "rank api"
-  ),
-  "tag" => "some_string_123",
-  "pingback_url" => 'https://your-server.com/pingscript?id=$id&tag=$tag'
-);
-// after a task is completed, we will send a GET request to the address you specify
-// instead of $id and $tag, you will receive actual values that are relevant to this task
-$post_array[] = array(
-  "location_name" => "United States",
-  "date_from" => "2019-01-01",
-  "date_to" => "2020-01-01",
-  "type" => "youtube",
-  "category_code" => 3,
-  "keywords" => array(
-    "seo api",
-    "rank api"
-  ),
-  "postback_url" => "https://your-server.com/postbackscript"
-);
-try {
-  // POST /v3/keywords_data/google_trends/explore/task_post
-  $result = $client->post('/v3/keywords_data/google_trends/explore/task_post', $post_array);
-  print_r($result);
-  // do something with post result
-} catch (RestClientException $e) {
-  echo "\n";
-  print "HTTP code: {$e->getHttpCode()}\n";
-  print "Error code: {$e->getCode()}\n";
-  print "Message: {$e->getMessage()}\n";
-  print  $e->getTraceAsString();
-  echo "\n";
-}
-$client = null;
+$curl = curl_init();
+
+curl_setopt_array($curl, array(
+    CURLOPT_URL => 'https://api.dataforseo.com/v3/serp/google/organic/live/advanced',
+    CURLOPT_RETURNTRANSFER => true,
+    CURLOPT_ENCODING => '',
+    CURLOPT_MAXREDIRS => 10,
+    CURLOPT_TIMEOUT => 0,
+    CURLOPT_FOLLOWLOCATION => true,
+    CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+    CURLOPT_CUSTOMREQUEST => 'POST',
+    CURLOPT_POSTFIELDS =>'[
+        {"keyword":"weather forecast", "location_code":2826, "language_code":"en", "device":"desktop", "os":"windows", "depth":100}
+    ]',
+    CURLOPT_HTTPHEADER => array(
+        'Authorization: Basic ZGFuLmJyaW5jYXQwN0BnbWFpbC5jb206YTY5MzI2YzNmODRjNjEzZA==',
+        'Content-Type: application/json'
+    ),
+));
+
+$response = curl_exec($curl);
+curl_close($curl);
+echo $response;
 ?>
