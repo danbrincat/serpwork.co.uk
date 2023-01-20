cc9e01ac6a2a44a5849c9f17eacc068a30fa27e4
danbrincat <dan.brincat07@gmail.com>
2023-01-20 15:49:08 +0000

    Update test.php

--- test.php
+++ test.php
@@ -3,25 +3,32 @@
 $curl = curl_init();
 
 curl_setopt_array($curl, array(
-  CURLOPT_URL => 'https://api.dataforseo.com/v3/keywords_data/google_ads/search_volume/live',
-  CURLOPT_RETURNTRANSFER => true,
-  CURLOPT_ENCODING => '',
-  CURLOPT_MAXREDIRS => 10,
-  CURLOPT_TIMEOUT => 0,
-  CURLOPT_FOLLOWLOCATION => true,
-  CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
-  CURLOPT_CUSTOMREQUEST => 'POST',
-  CURLOPT_POSTFIELDS =>'[
-    {"date_from":"2022-09-01", "date_to":"2022-12-31", "search_partners":false, "keywords":["weather forecast"], "location_code":2826, "language_code":"en", "sort_by":"relevance"}
-  ]',
-  CURLOPT_HTTPHEADER => array(
-    'Authorization: Basic ZGFuLmJyaW5jYXQwN0BnbWFpbC5jb206YTY5MzI2YzNmODRjNjEzZA==',
-    'Content-Type: application/json'
-  ),
+    CURLOPT_URL => 'https://api.dataforseo.com/v3/keywords_data/google_trends/explore/live',
+    CURLOPT_RETURNTRANSFER => true,
+    CURLOPT_ENCODING => '',
+    CURLOPT_MAXREDIRS => 10,
+    CURLOPT_TIMEOUT => 0,
+    CURLOPT_FOLLOWLOCATION => true,
+    CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+    CURLOPT_CUSTOMREQUEST => 'POST',
+    CURLOPT_POSTFIELDS =>'[
+        {"date_from":"2022-09-01", "date_to":"2022-12-31", "search_partners":false, "keywords":["weather forecast"], "location_code":2826, "language_code":"en", "sort_by":"relevance", "type":"web"}
+    ]',
+    CURLOPT_HTTPHEADER => array(
+        'Authorization: Basic ZGFuLmJyaW5jYXQwN0BnbWFpbC5jb206YTY5MzI2YzNmODRjNjEzZA==',
+        'Content-Type: application/json'
+    ),
 ));
 
 $response = curl_exec($curl);
+
+$decoded = json_decode($response);
+var_dump($response);
+var_dump($decoded);
+$encoded = json_encode($decoded);
+var_dump($encoded);
+
 curl_close($curl);
-echo $response;
+//echo $response;
 
 ?>
